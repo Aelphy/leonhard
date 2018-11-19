92cea3c9536653626aabc4b1f38491f1a925e7a6
Mikhail Usvyatsov <m.usvyatsov@gmail.com>
2018-11-19 17:18:37 +0100

    changed run scripts

--- run_cpu.sh
+++ run_cpu.sh
@@ -3,7 +3,7 @@
 module load python_gpu/3.6.0 # Avaliable versions are: 3.6.1 3.6.4 2.7.12 2.7.13 2.7.14
 module load opencv # If you need it
 # Amount of RAM per node - mem
-# span[ptile=20] is nessesary for multi-node job
+# span[ptile=20] is nessesary for multi-node job - indicates the number of processors on each host that should be allocated
 # -W time in hours which affects which queue will be selected
 # -n - number of cores per node
-bsub -n 1 -W 100:00 -R "rusage[mem=15096, ngpus_excl_p=1]" $PROGRAM_CMD
+bsub -n 1 -W 4:00 -R "rusage[mem=15096]" -R "span[ptile=10]" $PROGRAM_CMD
--- run_gpu.sh
+++ run_gpu.sh
@@ -6,7 +6,7 @@ module load opencv # If you need it
 # Number of gpus per node ngpus_excl_p
 # Amount of RAM per node - mem
 # The GPU nodes have 20 cores, 8 GPUs, and 256 GB of RAM (of which only about 210 GB is usable)
-# span[ptile=20] is nessesary for multi-node job
+# span[ptile=20] is nessesary for multi-node job - indicates the number of processors on each host that should be allocated
 # -W time in hours which affects which queue will be selected
 # -n - number of cores per node
-bsub -n 1 -W 100:00 -R "rusage[mem=8092, ngpus_excl_p=1] select[gpu_model1==GeForceGTX1080] span[ptile=10]" $PROGRAM_CMD
+bsub -n 1 -W 4:00 -R "rusage[mem=8092, ngpus_excl_p=1]" -R "select[gpu_model1==GeForceGTX1080]" -R "span[ptile=10]" $PROGRAM_CMD
